00b617cbbf6c0f464900a1c04b4664c473f84756
F-G Fernandez <26927750+frgfm@users.noreply.github.com>
2024-01-24 15:40:33 +0100

    docs: Updates the copyright year (#32)
    * docs: Adds FOSSA badge to README
    
    * docs: Updates copyright year

--- src/activation/activate.ts
+++ src/activation/activate.ts
@@ -1,4 +1,4 @@
-// Copyright (C) 2023, Quack AI.
+// Copyright (C) 2023-2024, Quack AI.
 
 // This program is licensed under the Apache License 2.0.
 // See LICENSE or go to <https://www.apache.org/licenses/LICENSE-2.0> for full license details.
